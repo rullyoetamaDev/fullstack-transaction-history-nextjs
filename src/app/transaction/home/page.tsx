8ecf73df54ff9d80b5aa0322a1fccd7c2416c7da
rullyoetamaDev <rullyhpfq2035au@gmail.com>
2024-06-22 08:04:57 +0700

    first commit

--- src/app/transaction/home/page.tsx
+++ src/app/transaction/home/page.tsx
@@ -1,18 +1,24 @@
 import { authOption } from "@/lib/auth";
-import { rupiah, tanggal } from "@/lib/utils";
+import { rupiah } from "@/lib/utils";
 import { getServerSession } from "next-auth";
-import { now } from "next-auth/client/_utils";
 import React from "react";
 
 const PageHomeTransaction = async () => {
   const session = await getServerSession(authOption);
-  //console.log("check : session di transaction : ",session)
+  const response = await fetch(`http://localhost:3000/api/transaction-history/${session?.user?.name}`);
+  const { user: data } = await response.json();
 
-  const res = await fetch(
-    `http://localhost:3000/api/transaction-history/${session?.user?.name}`
+  const renderRow = (dataJson: { description: string; status: string; amount: number; createdAt: string }, index: number) => (
+    <tr key={index}>
+      <td>{index + 1}</td>
+      <td>{dataJson.description}</td>
+      <td className={dataJson.status === "money in" ? 'text-green-300' : 'text-red-500'}>
+        {dataJson.status === "money in" ? rupiah(dataJson.amount) : `-${rupiah(dataJson.amount)}`}
+      </td>
+      <td>{dataJson.createdAt}</td>
+    </tr>
   );
-  const respones = await res.json();
-  const data = respones.user;
+
   return (
     <div>
       <h2 className="pb-3 font-semibold">Transaction History <span className="text-[#FD6245]">{`${session?.user?.name}`}</span></h2>
@@ -20,31 +26,13 @@ const PageHomeTransaction = async () => {
         <thead>
           <tr>
             <th>No</th>
-            <th>Description</th>
-            <th>Amount</th>
-            <th>CreatedAt</th>
+            <th>Deskripsi</th>
+            <th>Jumlah</th>
+            <th>Dibuat Pada</th>
           </tr>
         </thead>
         <tbody>
-          {data.map((dataJson: any, index : any) => {
-            return (
-              //console.log("check - dataJson : ",dataJson)
-              <tr key={index}>
-                <td>
-                  {index + 1}
-                </td>
-                <td>
-                  {dataJson.description}
-                </td>
-                <td className={dataJson.status === "money in" ? 'text-green-300' : 'text-red-500'}>
-                  {dataJson.status === "money in" ? rupiah(dataJson.amount) : "-" + rupiah(dataJson.amount) }
-                </td>
-                <td>
-                  {dataJson.createdAt}
-                </td>
-              </tr>
-            )
-          })}
+          {data.map(renderRow)}
         </tbody>
       </table>
     </div>
